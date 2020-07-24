a8fe25fb31ca2b0f7113b35841ff18eba83a35ed
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-07-24 18:13:57 +0530

    Update views.py

--- noSpam/views.py
+++ noSpam/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 
 def index(request) :
+    if request.method == "POST" :
+        print(request.POST.get('textarea'))
+        message = request.POST.get('textarea')
+        
     return render(request,"home.html",{"title":"Home"})
