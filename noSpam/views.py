722dd404cae3663f7b678d4e0da71bfa5298c5f8
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-07-24 18:39:54 +0530

    Update views.py

--- noSpam/views.py
+++ noSpam/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
+from .forms import PredictForm
 
 def index(request) :
+    form = PredictForm(request.POST or None)
     if request.method == "POST" :
-        print(request.POST.get('textarea'))
-        message = request.POST.get('textarea')
+        #print(request.POST.get('textarea'))
+        #message = request.POST.get('textarea')
+        # if form.is_valid() :
+        #     return HttpResponseRedirect('/thanks/')
+        message = request.POST.get('message')
+        print(message)
         
-    return render(request,"home.html",{"title":"Home"})
+    return render(request,"home.html",{"title":"Home","form":form})
