45258c458d823c834e16c232463d881c1b429ef5
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-07-25 12:07:06 +0530

    Update forms.py

--- noSpam/forms.py
+++ noSpam/forms.py
@@ -2,14 +2,16 @@ from django import forms
 
 class PredictForm(forms.Form) :
     message = forms.CharField(
+                                label='',
                                 widget=forms.Textarea(
                                     attrs={
-                                        "class" :"new-class-name two",
+                                        "class" :"mx-auto",
                                         "id" : "textarea",
                                         "name" : "textarea", 
-                                        "rows" : 8,
-                                        "cols" : 40, 
+                                        "rows" : 10,
+                                        "cols" : 50, 
                                         "placeholder" : "Enter your sms here", 
+
                                     }
                                 )
     )
