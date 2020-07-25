d766f188727129100f87f27e55f91c8045f9c3f0
Vijay Jaisankar <vijay.jaisankar@iiitb.org>
2020-07-25 17:44:18 +0530

    Update views.py

--- noSpam/views.py
+++ noSpam/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+import turicreate
 
 from .forms import PredictForm
 
@@ -11,3 +12,12 @@ def index(request) :
     
     form = PredictForm()
     return render(request,"home.html",{"title":"Home","form":form})
+
+# Changes made here
+def predictValue(s):
+    data = turicreate.SFrame.read_csv('SMSSpamCollection', header=False, delimiter='\t', quote_char='\0')
+    data = data.rename({'X1': 'label', 'X2': 'text'})
+    train, test = data.random_split(0.8)
+    model = turicreate.text_classifier.create(train, 'label', features=['text'], max_iterations=10000)
+    x = turicreate.SFrame({'text':[s]})
+    return model.predict(x)
